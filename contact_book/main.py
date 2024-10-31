b7b3164e6de05fa20f2bf32308b2f3570b87fc88
wtc <wtc@pop-os.localdomain>
2024-10-31 14:35:56 +0200

    Done

--- contact_book/main.py
+++ contact_book/main.py
@@ -1,2 +1,11 @@
 #use python dictionaries to create a function that prompts a user to enter their name and number and 
-#display what the contact book looks like at the moment 
+#display what the contact book looks like at the moment
+
+def contact_book(name, phone):
+    book = {}
+    book["name"] = name
+    book["phone"] = phone
+    
+    return book
+
+print(contact_book("Ayanda", "0743283003"))
